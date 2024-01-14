07cd3f35a95a9736c106dd6ec7113034c0ab81ce
Glam52 <gla_max@outlook.com>
2024-01-14 17:09:44 +0700

    переработка even_game

--- brain_games/even_game.py
+++ brain_games/even_game.py
@@ -1,41 +1,20 @@
 from random import randint
-from Core import welcome_user
+from brain_games.Core import game_core
 
-# Приветствие
-name = welcome_user()
-
-
-# Функция проверки на четность
-def is_even(n: int) -> str:
-    if n % 2 == 0:
-        return 'yes'
+def game_generator():
+    question = [randint(1, 99)]
+    if question[0] % 2 == 0:
+        answer = ['yes']
     else:
-        return 'no'
+        answer = ['no']
+
+    question = " ".join(str(x) for x in question)
+    answer = " ".join(str(x) for x in answer)
+
+    return question, answer
 
 
-def even():
-    # Строка с правилами игры
-    print('Answer "yes" if the number is even, otherwise answer "no".')
+description = 'Answer "yes" if the number is even, otherwise answer "no".'
 
-    count = 0  # счетчик ответов
-    # основной цикл игры
-    for _ in range(3):
+game_core(game_generator, description)
 
-        rand_x = randint(1, 999)  # новое число для пользователя
-
-        print('Question:', rand_x)
-
-        answer = input('Your answer: ')  # получаем ответ пользователя
-
-        # сравниваем ответ и функцию-проверку
-        if answer != is_even(rand_x):
-            print(f"'{answer}' is wrong answer ;(. "
-                  f"Correct answer was '{is_even(rand_x)}'\n"
-                  f"Let's try again, {name}")
-            break
-        else:
-            print('Correct!')
-            count += 1
-
-    if count == 3:
-        print(f'Congratulations, {name}')
